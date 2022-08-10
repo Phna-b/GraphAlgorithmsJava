38af1c8daeaf9189425345417c5f6f6bbacad5eb
Pedro Henrique Nunes de Assis <phna8589@gmail.com>
2022-08-10 22:24:38 +0000

    Att

--- Main.java
+++ Main.java
@@ -13,6 +13,8 @@ class Main {
     g1.addEdge(0, 3, 4);
     g1.addEdge(3, 0, 4);
     g1.addEdge(3, 4, 2); // Error.
+    g1.addEdge(3, 3, 4);
+    g1.addEdge(3, 2, 4);
 
     // Teste Degree
     System.out.println(g1);
@@ -20,8 +22,9 @@ class Main {
     System.out.println(g1.degree(3));
 
     // Teste maior grau
+    System.out.println("\nMaior grau " + g1.highestDegree());
+    
     System.out.println(g1);
-    System.out.println("Maior grau " + g1.highestDegree());
 
   }
 }
